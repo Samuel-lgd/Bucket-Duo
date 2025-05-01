f4509f41dc4067d6a69a1c1c035fa6acf5b65dff
Samuel-lgd <lagarde.samuel@gmail.com>
2025-05-01 19:43:43 +0200

    add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Bucket"]["type"]>>([]);
+  const [todos, setBuckets] = useState<Array<Schema["Bucket"]["type"]>>([]);
 
   useEffect(() => {
     client.models.Bucket.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
+      next: (data) => setBuckets([...data.items]),
     });
   }, []);
+
+  const { signOut } = useAuthenticator();
 
   function createTodo() {
     client.models.Bucket.create({ name: window.prompt("Give a name to your bucket")})
@@ -19,13 +22,14 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>My Buckets</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.name}</li>
         ))}
       </ul>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
